d843fb58e7811366b8bcc671d6acf39dc2811c11
alkalariyouthscampus <bunuyakubu10@gmail.com>
2026-07-29 15:18:53 +0100

    Add welcome message and load notification

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    alert("Welcome to Alkalari Community Foundation for Youth Development (ACFYD)!");
+}
+
+window.onload = function () {
+    console.log("ACFYD Website Loaded Successfully");
+};
